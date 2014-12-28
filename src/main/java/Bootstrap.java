1f7d9489054f06f1865c5ee7a4e2d6fa9ab04c5a
jscattergood <john.scattergood@gmail.com>
2014-12-28 14:52:32 -0800

    initially commit project files

---
+++ src/main/java/Bootstrap.java
@@ -0,0 +1,11 @@
+import com.netflix.ribbon.Ribbon;
+
+/**
+ * @author <a href="mailto:john.scattergood@navis.com">John Scattergood</a> 12/27/2014
+ */
+public class Bootstrap {
+    public static void main(String[] args) {
+        Ribbon.createHttpResourceGroup("")
+    }
+
+}
